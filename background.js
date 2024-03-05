0998903cf3c3250d5ed5cdafb9e735e216dbf6d6
Sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-03-05 15:13:21 +0530

    init

---
+++ background.js
@@ -0,0 +1,11 @@
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.webRequest.onBeforeRequest.addListener(
+        function (details) {
+            if (details.url.startsWith("https://www.reddit.com/")) {
+                return { redirectUrl: details.url.replace("https://www.reddit.com/", "https://new.reddit.com/") };
+            }
+        },
+        { urls: ["<all_urls>"] },
+        ["blocking"]
+    );
+});
